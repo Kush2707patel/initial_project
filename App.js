5b438ed40338ea863cf9d7f1b3116dfb2185b13b
kush2707patel <kush3601@gmail.com>
2024-03-14 10:47:41 +0530

    initial project

--- App.js
+++ App.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const app =express();
-var bodyParser=require("body-parser");      
-app.use(bodyParser.json());  // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-//importing the routes from other files       
 console.log(App.js);                  
